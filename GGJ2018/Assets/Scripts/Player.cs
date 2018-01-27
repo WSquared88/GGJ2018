80f47e1335d5f991f56acefcf8ce4ab16ea5d1dc
Chase Slattery (RIT Student) <chs6298@ad.rit.edu>
2018-01-26 22:15:26 -0500

    Added checking for player object collision, as well as added basic ability to add values to an interactable object

---
+++ GGJ2018/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "interactable")
+        { 
+            InteractableObject interact = other.GetComponent<InteractableObject>(); 
+            if((gameObject.transform.position - other.gameObject.transform.position).magnitude <= interact.seeDistance)
+            {
+                Debug.Log(interact.giveStats()[1]);
+            }
+        }
+    }
+}
